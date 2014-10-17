cd91e33d6b3bcf27619144b97607172d4afcd40c
seanrlarge <sean.large1@gmail.com>
2014-10-17 14:17:26 -0400

    added some new resources to routes.rb changes are commented out but will need them later

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :blogs
-
+# do get 'drafts', on: :collection
+# get 'search', on: :collection
+# end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

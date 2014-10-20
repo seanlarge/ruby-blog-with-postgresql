b85335cecb727ed0a3138ebb1fa423fd47d85377
seanrlarge <sean.large1@gmail.com>
2014-10-19 20:05:21 -0400

    search function working

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,13 +1,8 @@
 class Blog < ActiveRecord::Base
 
-def self.search(search)
-  if search
-    find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
-  else
-    find(:all)
-  end
-  # sort: display
-
-end
+  scope :draft, -> { where(draft_status: true)}
+  scope :published, -> { where(draft_status: false)}
+  scope :search, -> (search_query) { where('body LIKE ? OR subject LIKE ?', "%#{search_query}%", "%#{search_query}%") }
+  validates :body, presence: true
 
 end # end of class blog
